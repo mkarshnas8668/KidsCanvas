75822af3b8db5f712ae1ac77cea5eb72babf2f65
Mohammad karshnas <mkarshnas6@gmail.com>
2025-03-25 05:51:07 +0330

    add 31 image for coloring v1

--- app/src/main/java/com/mkarshnas6/karenstudio/kidscanvas/ColoringActivity.kt
+++ app/src/main/java/com/mkarshnas6/karenstudio/kidscanvas/ColoringActivity.kt
@@ -26,7 +26,39 @@ class ColoringActivity : AppCompatActivity() {
 
         // Creating a list of ImageItems (with Persian and English names)
         val imageList = listOf(
-            ImageItem("گل 1", "flower1")
+            ImageItem("گل 1", "flower1"),
+            ImageItem("مرد عنکبوتی 1", "spiderman1"),
+            ImageItem("مرد عنکبوتی 2", "spiderman2"),
+            ImageItem("مرد عنکبوتی 3", "spiderman3"),
+            ImageItem("مرد عنکبوتی 4", "spiderman4"),
+            ImageItem("شیر 1", "line1"),
+            ImageItem("شیر 2", "lion2"),
+            ImageItem("پرنده 1", "bird1"),
+            ImageItem("قروزن 1", "frozen1"),
+            ImageItem("فروزن 2", "frozen2"),
+            ImageItem("مینیون 1", "minion1"),
+            ImageItem("مینیون 2", "minion2"),
+            ImageItem("پاندا 1", "panda1"),
+            ImageItem("پاندا 2", "panda2"),
+            ImageItem("کیتی 1", "kitty1"),
+            ImageItem("کیتی 2", "kitty2"),
+            ImageItem("کیتی 3", "kitty3"),
+            ImageItem("کیتی 4", "kitty4"),
+            ImageItem("گروت 1", "groot1"),
+            ImageItem("ایرون من 1", "ironman1"),
+            ImageItem("ایرون من 2", "ironman2"),
+            ImageItem(" سونیک 1", "sonic1"),
+            ImageItem("سونیک 2", "sonic2"),
+            ImageItem("سونیک 3", "sonic3"),
+            ImageItem("میکی موس 1", "mickeymouse1"),
+            ImageItem("میکی موس 2", "mickeymouse2"),
+            ImageItem("سوپر ماریو 1", "supermario1"),
+            ImageItem("اساسین کرید 1", "assassinscreed1"),
+            ImageItem("ریک اند مورتی 1", "rickandmorty1"),
+            ImageItem("اسنوپی 1", "snoopy1"),
+            ImageItem("اسنوپی 2", "snoopy2"),
+            ImageItem("اسنوپی 3", "snoopy3"),
+
         )
 
         // Set the LayoutManager for the RecyclerView
